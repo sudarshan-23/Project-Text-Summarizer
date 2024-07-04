6aa0ac57733af9d6ab1f558ad3f7d8ebaa891562
sudarshan-23 <sudarshanjs23@gmail.com>
2024-07-05 01:15:55 +0530

    updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = "0.0.0"
 
 REPO_NAME = "Project-Text-Summarizer"
 AUTHOR_USER_NAME = "sudarshan-23"
-SRC_REPO = "text-summarizer"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "sudarshanjs23@gmail.com"
 
 setuptools.setup(
